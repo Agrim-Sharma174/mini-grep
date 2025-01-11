e61bde340bfa63629c67041825e9606a55c2e6e1
Agrim <agrimsharma1714@gmail.com>
2025-01-11 23:55:25 +0530

    refactor: reading arguments and file

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // let query = &args[1];
+    // let file_path = &args[2];
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In the file {}", config.file_path);
+
+    let content = fs::read_to_string(config.file_path).expect("Should have been able to read the code.");
+
+    println!("The text is:\n{content}");
+
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config {query, file_path}
+}
