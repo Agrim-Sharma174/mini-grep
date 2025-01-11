08919e6f3b764e8dda55ef53e237156dc6e747d1
Agrim <agrimsharma1714@gmail.com>
2025-01-11 23:58:30 +0530

    constructor and error handling

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ fn main() {
 
     // let query = &args[1];
     // let file_path = &args[2];
-    let config = parse_config(&args);
+    // let config = parse_config(&args);
+
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In the file {}", config.file_path);
@@ -19,6 +21,18 @@ fn main() {
 struct Config {
     query: String,
     file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
 
 fn parse_config(args: &[String]) -> Config {
