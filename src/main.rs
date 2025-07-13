e494b8240b62524a30650753a53e0eb8e7c8add7
Agrim <agrimsharma1714@gmail.com>
2025-07-13 21:05:52 +0530

    feat: made the library crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+use minigreprust::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,31 +12,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In the file {}", config.file_path);
 
-    run(config);
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content =
-        fs::read_to_string(config.file_path)?;
-
-    println!("The text is:\n{content}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = minigreprust::run(config) {
+        println!("Appliccation error: {e}");
+        process::exit(1);
     }
+    
 }
+
+
